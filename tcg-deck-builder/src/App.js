6801fd4b373dbf2185a4605fe623c0660e0bbef6
Tishinator <mike.tishman@gmail.com>
2024-01-20 21:00:06 -0500

    Added dark mode toggle

--- tcg-deck-builder/src/App.js
+++ tcg-deck-builder/src/App.js
@@ -1,12 +1,31 @@
 import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Header from './components/layout/Header';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(    
+    localStorage.getItem('darkMode') === 'true' ||
+    window.matchMedia('(prefers-color-scheme: dark)').matches);
+  
+
+  useEffect(()=>{
+    // Apply the dark mode class to the body
+    document.body.classList.toggle('dark-mode', darkMode);
+
+    // Store the user's preference in localStorage
+    localStorage.setItem('darkMode', darkMode);
+  },[darkMode]);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  }
   return (
     <div className="App">
-      <Header />
+      <div id="content" className={darkMode ? 'dark-mode' : ''}>
+        <Header darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
+      </div>
     </div>
   );
 }
--- tcg-deck-builder/src/components/layout/Header.js
+++ tcg-deck-builder/src/components/layout/Header.js
@@ -1,12 +1,20 @@
 import Container from 'react-bootstrap/Container';
 import NavBar from 'react-bootstrap/Navbar';
+import { FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
 
-function Header(){
+
+function Header({darkMode, toggleDarkMode}){
     return (
-        <NavBar variant='dark' bg='dark'>
-            <Container>
+        <NavBar bg={darkMode?"dark" : "light"} data-bs-theme={darkMode?"dark" : "light"}  fixed='top'>
+            <Container style={{ paddingLeft: '20px' }} fluid='true'>
                 <NavBar.Brand>Pokemon TCG Deck Builder</NavBar.Brand>
             </Container>
+            <NavBar.Collapse className='justify-content-end'>
+                <Container fluid="true" style={{ paddingRight: '20px' }}>
+                    {darkMode ? <FontAwesomeIcon icon={faMoon} onClick={toggleDarkMode}/> : <FontAwesomeIcon icon={faSun} onClick={toggleDarkMode}/>}
+                </Container>
+            </NavBar.Collapse>
         </NavBar>
     );
 };
